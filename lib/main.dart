adaf6b0d9ca0bdc03189bb0bfd40f577ab92df43
Koichi5 <koichi20021217@gmail.com>
2025-02-10 01:08:12 +0900

    feat: Add profile page with Golden Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cursor_agent_sample/views/my_home_page/my_home_page.dart';
+import 'package:cursor_agent_sample/views/profile_page/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        useMaterial3: true,
         brightness: Brightness.light,
         colorSchemeSeed: Colors.deepPurple,
       ),
@@ -22,7 +24,63 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         colorSchemeSeed: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const MyHomePage(title: 'Flutter Demo'),
+    const ProfilePage(title: 'プロフィール'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: [
+          NavigationDestination(
+            icon: Icon(
+              Icons.home_outlined,
+              color: theme.colorScheme.onSurfaceVariant,
+            ),
+            selectedIcon: Icon(
+              Icons.home_rounded,
+              color: theme.colorScheme.onSecondaryContainer,
+            ),
+            label: 'ホーム',
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.person_outline_rounded,
+              color: theme.colorScheme.onSurfaceVariant,
+            ),
+            selectedIcon: Icon(
+              Icons.person_rounded,
+              color: theme.colorScheme.onSecondaryContainer,
+            ),
+            label: 'プロフィール',
+          ),
+        ],
+      ),
     );
   }
 }
